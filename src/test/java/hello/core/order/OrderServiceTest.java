c91c7beb374e5119efbbd3bca3c24604321bc298
lee0119 <candogo91@gmail.com>
2022-09-03 01:50:58 +0900

    Spring

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    //void 의 의미는?
+    void creatOrder(){
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        orderService.createOrder((memberId), "itemaA", 10000);)
+
+        Assertions.assertThat(order.getDiscountPrice().isEqualTo(1000));
+//        Assertions.assertT
+
+    }
+}
