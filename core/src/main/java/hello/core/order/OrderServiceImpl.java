8dfee21ffb9f18031fd800ee3e635e7ddae74aa4
lee0119 <candogo91@gmail.com>
2022-09-03 01:38:40 +0900

    Spring

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,28 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findbyId(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+            //여기 왜 member, itemprice 인지 price가 아니라
+
+        return new Order(memberId, itemName , itemPrice, discountPrice);
+
+
+    }
+}
+
+
+
