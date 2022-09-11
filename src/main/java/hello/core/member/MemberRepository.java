eeb4d9447bc59ce5a4c5621fd8a8b7592f6494e8
lee0119 <candogo91@gmail.com>
2022-09-12 00:46:18 +0900

    Spring

--- src/main/java/hello/core/member/MemberRepository.java
+++ src/main/java/hello/core/member/MemberRepository.java
@@ -5,7 +5,7 @@ public interface MemberRepository {
     void save(Member member);
 
 
-    //멤버 찾아오기 더 연습 연구 
+    //멤버 찾아오기 더 연습 연구
     Member findbyId(Long memberId);
 
 }
--- src/main/java/hello/core/order/OrderService.java
+++ src/main/java/hello/core/order/OrderService.java
@@ -1,5 +1,8 @@
 package hello.core.order;
 
+
+
+//서비스 공부 다시하기
 public interface OrderService   {
     Order createOrder(Long memberId, String itemName, int itemPrice);
 
