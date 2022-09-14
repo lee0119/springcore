1e9f1156860726a1d3855a1668e54150ae8fbe90
lee0119 <candogo91@gmail.com>
2022-09-15 00:04:35 +0900

    Spring

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -2,25 +2,39 @@ package hello.core.beanfind;
 
 import hello.core.AppConfig;
 import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 public class ApplicationContextSameBeanFindTest {
 
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
 
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
     void findBeanByTypeDuplicate() {
-        ac.getBean(DiscountPolicy.class);
+        MemberRepository bean = ac.getBean(MemberRepository.class);
     }
 
-    //static 쓰는 이유 지금 클래스에서만 쓰겠다. 
+    //static 쓰는 이유 지금 클래스에서만 쓰겠다.
     @Configuration
     static class SameBeanConfig {
 
+
+        @Bean
+        public MemberRepository memberRepository() {
+            return new MemoryMemberRepository();
+        }
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
     }
+
+
 }
